cd4daa5a575476008b767c8bfb273f20f48405a3
Wes Reid <bwreid@gmail.com>
2018-01-08 13:46:14 -0800

    Update ArrivalTime.js

--- src/components/ArrivalTime.js
+++ src/components/ArrivalTime.js
@@ -38,35 +38,7 @@ class ArrivalTime extends Component{
     )
   }
 }
-// const ArrivalTime = () => {
 
-  // turn to class and use componentDidMount
-
-  // window.$('#input_starttime').pickatime({
-  //     twelvehour: true
-  // });
-
-//   return (
-//     <div className="body">
-//       <h2 className="py-4 mb-5 title font-weight-bold">create a schedule</h2>
-//       <img className="selection-img mb-2" src="./img/branding/days-of-the-week-orange.svg" />
-//       <h5 className="px-5">when do you need to be at your destination?</h5>
-//       <small className="mb-5 pb-5">aka work, the gym, the opera, HH</small>
-//       <div className="container">
-//         <div className="md-form mt-5">
-//           <input placeholder="select a time" type="text" id="input_starttime" className="form-control timepicker"/>
-//         </div>
-//       </div>
-//
-//       <div className="footer-container py-3">
-//         <Link to={'/configuring'}>
-//           <NextButton />
-//         </Link>
-//       </div>
-//     </div>
-//   )
-// }
-
-
+// No dead code!
 
 export default ArrivalTime
